8bf32ecd5c66b83e8e346ceb9a3ae509e6f1afbd
glezo1 <danielgonzalezmarcos@hotmail.com>
2017-03-27 23:50:49 +0200

    Create mysql_backup.sh
    backup tool: it creates a directory with date in format yyyyMMdd, which will store two files per database: structure and data

---
+++ mysql_backup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+####################################################################################################
+#2016/03/27, @glezo1                                                                               #
+#dump every database into two files: structure and data.                                           #
+#It won't dump 'information_schema' or 'mysql' databases                                           #
+#yes, I know. I shouldn't do the plain old -p$password. Just a quick'n dirty backup.               #
+####################################################################################################
+
+#CONSTANTS####################################
+db_host="localhost"
+db_user="root"
+db_pass="you_know_bro_this_aint_my_password"
+backup_parent_directory="/root/mysql_backup"
+##############################################
+
+mkdir -p $backup_parent_directory
+
+now_string=`date +"%Y%m%d"`
+output_directory=$backup_parent_directory/$now_string
+mkdir -p $output_directory
+
+databases_to_dump=`mysql -h $db_host -u $db_user -p$db_pass -e "show databases" | grep -Ev 'Database|information_schema|mysql'`
+for current_db in $databases_to_dump
+do
+  echo $current_db
+  mysqldump -h $db_host -u $db_user -p$db_pass --events --routines --triggers --no-data $current_db > ${output_directory}/$current_db"_STRUCTURE.sql"
+  mysqldump -h $db_host -u $db_user -p$db_pass --no-create-info --skip-triggers $current_db         > ${output_directory}/$current_db"_DATA.sql"
+done
