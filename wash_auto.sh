c127a86ba50bb76882b7cbd3ac4fee595b4d37dc
glezo1 <danielgonzalezmarcos@hotmail.com>
2017-04-25 09:30:21 +0200

    Update wash_auto.sh

--- wash_auto.sh
+++ wash_auto.sh
@@ -5,17 +5,17 @@ set timeout	""
 set bssid	""
 set iface	""
 set channel	""
-#this shall contain 'operands', as timeout, bssid, iface, and, optionally, channel
-set operand  	""
+#this shall contain 'operators', as timeout, bssid, iface, and, optionally, channel
+set operator  	""
 for {set x 0} {$x<$argc} {incr x} {
 	if {$x%2==0} {
-		set operand [lindex $argv $x]
+		set operator [lindex $argv $x]
 	}
 	if {$x%2!=0} {
-		if {$operand=="timeout"}	{set timeout	[lindex $argv $x]}
-		if {$operand=="bssid"}		{set bssid	[lindex $argv $x]}
-		if {$operand=="iface"}		{set iface	[lindex $argv $x]}
-		if {$operand=="channel"}	{set channel	[lindex $argv $x]}
+		if {$operator=="timeout"}	{set timeout	[lindex $argv $x]}
+		if {$operator=="bssid"}		{set bssid	[lindex $argv $x]}
+		if {$operator=="iface"}		{set iface	[lindex $argv $x]}
+		if {$operator=="channel"}	{set channel	[lindex $argv $x]}
 	}
 }
 
